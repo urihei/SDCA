a26f129b682ad2f32edd8b0902ba5851f53ff74e
urihei <urihei@cs.huji.ac.il>
2014-12-29 13:17:44 +0200

    adding predefined

---
+++ preKernelSVM.hpp
@@ -0,0 +1,26 @@
+#ifndef _kernelSVM__
+#define _kernelSVM__
+#include "svm.hpp"
+
+
+
+class kernelSVM: public svm{
+public:
+    kernelSVM(ivec &y,matd &data,size_t k,
+              double lambda=1, double gamma=1,
+              unsigned int iter=50,unsigned int accIter=0);
+    
+    virtual void learn_SDCA(mat &alpha, mat &zAlpha);
+    virtual void learn_acc_SDCA();
+    virtual void classify(matd data,ivec &res);
+    virtual void saveModel(string fileName);
+    
+protected:
+
+    size_t _n;
+
+    mat _alpha;
+    mat _kernel;
+    
+};
+#endif
